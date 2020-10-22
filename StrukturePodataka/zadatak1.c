b30de6a46c4a97367ddc6470883b4af70b3e1f70
culjakluka <llukaculjak@gmail.com>
2020-10-22 13:50:00 +0200

    first exercise commit

--- StrukturePodataka/zadatak1.c
+++ StrukturePodataka/zadatak1.c
@@ -6,35 +6,89 @@ typedef struct Student student;
 struct Student {
 	char ime[50];
 	char prezime[50];
-	unsigned int bodovi;
+	int bodovi;
 };
-int countStudents(char* fileName, int* n);
-int readFromFile(char* filename, int n);
+
+int countStudents(char*, int*);
+int allocateMemory(student**, int);
+int readFromFile(char*, student*, int);
+int outputStudents(student*, int);
+
 int main() {
-	student* st;
+	student *st = NULL;
 	int numStud = 0;
 
 	if (!countStudents("studenti.txt", &numStud)) {
 		printf("\ncountStudents error");
 		return -1;
 	}
-	st = (student*)malloc(numStud * sizeof(student));
 
+	if (!allocateMemory(&st, numStud)) {
+		printf("\nallocateMemory error");
+		return -1;
+	}
+
+	if (!readFromFile("studenti.txt", st, numStud)) {
+		printf("\nreadFromFile error");
+		return -1;
+	}
+
+	if (!outputStudents(st, numStud)) {
+		printf("\noutputStudent error");
+		return -1;
+	}
+	
+	free(st);
 	return 0;
 }
 
-int countStudents(char* fileName, int* n) {
-	FILE* fp;
+int countStudents(char* fileName,int *n) {
+	FILE* fp = NULL;
 	char c;
 	
-	fp = fopen(fileName, "r");
-	if (fp == NULL) {
+	if (!(fp = fopen(fileName, "r"))) {
 		perror(fp);
 		return 0;
 	}
-	for (c = getc(fp); c == EOF; c = getc(fp))
+	c = getc(fp);
+	if(c=='\n') return 1;
+	(*n)++;
+	while (c != EOF) { 
 		if (c == '\n') (*n)++;
-
+		c = getc(fp);
+	}
 	fclose(fp);
+	return 1;
+}
+
+int allocateMemory(student** st, int numStud) {
+	*st = (student*)malloc(numStud * sizeof(student*));
+	if (st == NULL) return 0;
+	return 1;
+}
+
+int readFromFile(char* fileName, student* stArr, int n) {
+	FILE* fp = NULL;
+
+	if (!(fp = fopen(fileName, "r"))) {
+		perror(fp);
+		return 0;
+	}
+	for (int i = 0; i < n; i++) {
+		if (!fscanf(fp, "%s %s %d", stArr[i].ime, stArr[i].prezime, &stArr[i].bodovi)) {
+			printf("\nfscanf error");
+			return 0;
+		}
+	}
+	fclose(fp);
+	return 1;
+}
+
+int outputStudents(student* stArr, int n) {
+	int i;
+
+	for (i = 0; i < n; i++) {
+		printf("\n %s %s %d %d", stArr[i].ime, stArr[i].prezime, stArr[i].bodovi, (stArr[i].bodovi/100)*100);
+	}
 	return 1;
 }
