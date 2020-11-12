a26bc1c52da44bc4c7ef7c65487a2d024ddbb043
culjakluka <68790066+culjakluka@users.noreply.github.com>
2020-11-12 17:59:20 +0100

    Update zadatak4.c

--- StrukturePodataka/zadatak4.c
+++ StrukturePodataka/zadatak4.c
@@ -14,6 +14,7 @@ struct poly {
 };
 
 Position createNode(int coef, int expo);
+void createList(Position head, Position what);
 void addPolynomials(Position p, Position q, Position r);
 void addNode(Position head, Position what);
 void printList(Position head);
@@ -29,43 +30,64 @@ int main() {
 	head2.coef = 0;
 	head2.expo = 0;
 	r.next = NULL;
-	readFile("poly1.txt", head1.next);
+
+	readFile("poly1.txt", &head1);
 	printList(head1.next);
-	readFile("poly2.txt", head2.next);
+
+	readFile("poly2.txt", &head2);
 	printList(head2.next);
+
 	addPolynomials(head1.next, head2.next, &r);
 	printList(r.next);
+
 	return 0;
 }
 Position createNode(int coef, int expo) {
 	Position el = NULL;
 	el = (Position)malloc(sizeof(Poly));
+	if (NULL == el) {
+		return NULL;
+	}
 	el->coef = coef;
 	el->expo = expo;
 	el->next = NULL;
 	return el;
 }
+void createList(Position head, Position what) {
+	if (NULL == head->next) {
+		what->next = NULL;
+		head->next = what;
+		return;
+	}
+	head = head->next;
+	for (head; head != NULL; head = head->next) {
+		if (what->expo > head->expo) {
+			what->next = head->next;
+			head->next = what;
+		}
+	}
+}
 void addNode(Position head, Position what) {
 	Position p = head;
 	if (what->coef == 0)
 		return;
-	//here
 
-	while (NULL != p) {
-		if (what->expo < p->expo)
-			p = p->next;
-		else if (what->expo == p->expo) {
-			p->coef += what->coef;
-			if (0 == p->coef)
-				deleteNode(head, p);
-			return;
-		}
-		else {
-			what->next = p->next;
-			p->next = what;
-			return;
-		}
+	p = p->next;
+	if (NULL == p) {
+		what->next = NULL;
+		head->next = what;
 	}
+	else 
+		for (p; p != NULL; p = p->next) {
+		/*if (p->expo == what->expo) {
+			p->coef += what->coef;
+			return;
+		}*/
+			 if ((what->expo > p->expo)) {
+				what->next = NULL;
+				p->next = what;
+			}
+		}
 }
 void deleteNode(Position head, Position what) {
 	Position p = findPrevious(head, what);
@@ -78,24 +100,34 @@ void deleteNode(Position head, Position what) {
 }
 Position findPrevious(Position head, Position what) {
 	Position p = head;
+	if (NULL == what) {
+		while (!(NULL == p->next))
+			p = p->next;
+		return p;
+	}
 	while (!(p->next->expo == what->expo)) {
 		p = p->next;
 	}
 	return p;
 }
 void addPolynomials(Position p, Position q, Position r) {
+	Position el = NULL;
 	while (NULL != p && NULL != q) {
 		if (p->expo < q->expo) {
-			addNode(r, p);
+			el = createNode(p->coef, p->expo);
+			addNode(r, el);
 			p = p->next;
 		}
 		else if (p->expo > q->expo) {
+			el = createNode(q->coef, q->expo);
 			addNode(r, q);
 			q = q->next;
 		}
 		else if (p->expo == q->expo) {
-			addNode(r, p);
-			addNode(r, q);
+			if (p->coef + q->coef != 0) {
+				el = createNode(p->coef + q->coef, p->expo);
+				addNode(r, el);
+			}
 			p = p->next;
 			q = q->next;
 		}
@@ -120,7 +152,7 @@ void readFile(char* fileName, Position pos) {
 	}
 	while (!feof(fp)) {
 		fscanf(fp, " %d %d", &coef, &expo);
-		addNode(pos, createNode(coef, expo));
+		createList(pos, createNode(coef, expo));
 	}
 	fclose(fp);
 }
