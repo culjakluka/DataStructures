2265032020260556cb75c6fd294c56bcf68d5ddd
culjakluk <llukaculjak@gmail.com>
2021-01-21 15:51:21 +0100

    added 8th task

--- Zavrseni zadaci/zadatak8.c
+++ Zavrseni zadaci/zadatak8.c
@@ -1,81 +1,240 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
+#include <malloc.h>
 #include <string.h>
-#define DIR_NAME_LENGTH 1024
-#define err puts("ERROR")
 
-struct _dir;
-struct stack;
-typedef struct _dir Dir;
-typedef struct _dir* Position;
-typedef struct stack Stack;
-typedef struct stack* Pos;
-
-struct _dir {
-	char Name[DIR_NAME_LENGTH];
-	Position next;
+struct Dir;
+typedef struct Dir* Position;
+typedef struct Dir Dir;
+typedef struct Dir {
+	char* dirname;
+	Position sibling;
 	Position child;
 };
-struct stack {
-	Position prev;
-	Pos next;
+
+struct stack;
+typedef struct stack* StackPosition;
+typedef struct stack Stack;
+typedef struct stack {
+	Position Dir;
+	StackPosition next;
 };
 
-void MakeDirectory(Position);
-Position CreateNewDirectory();
-void CreateSortedList(Position, Position);
+int MenuChoice(char*);
+Position AddDirectory(Position, char*);
+int SubdirectoryPrint(Position);
+Position Subdirectory(Position, char*, StackPosition);
+int Push(Position, StackPosition);
+Position ParentDirectory(Position, StackPosition);
 
-int main() {
-	Dir head;
-	strcpy(head.Name, "C:");
-	head.next = NULL;
-	head.child = NULL;
-	int action = 0;
-	Position current = &head;
+int main()
+{
+	Dir root;
+	root.sibling = NULL;
+	root.child = NULL;
+	char* cmd_line = NULL;
+	char* buff = NULL;
+	int pos = 0;
+	Position last = NULL;
+	Stack stack;
+	stack.Dir = NULL;
+	stack.next = NULL;
+	StackPosition NewNode = NULL;
 
-	while (action!=5)
+
+	root.dirname = (char*)malloc(4 * sizeof(char));
+	if (NULL == root.dirname)
+		printf("An error occured!\n");
+
+	strcpy(root.dirname, "Root");
+
+	last = &root;
+
+	cmd_line = (char*)malloc(30 * sizeof(char));
+	if (NULL == cmd_line)
+		printf("An error occured!\n");
+
+	memset(cmd_line, 0, 30);
+
+	buff = (char*)malloc(30 * sizeof(char));
+	if (NULL == buff)
+		printf("An error occured!\n");
+
+	memset(buff, 0, 30);
+
+	NewNode = (StackPosition)malloc(sizeof(Stack));
+	if (NULL == NewNode)
+		printf("An error occured!\n");
+
+	NewNode->next = NULL;
+	NewNode->Dir = NULL;
+
+	NewNode->Dir = last;
+	NewNode->next = stack.next;
+	stack.next = NewNode;
+
+	while (strcmp(buff, "exit"))
 	{
-		switch (action)
+		memset(cmd_line, 0, 30);
+		gets(cmd_line);
+		sscanf(cmd_line, "%s%n", buff, &pos);
+
+		switch (MenuChoice(buff))
 		{
-		case 1:
-			MakeDirectory(current);
+		case 1://md
+			memset(buff, 0, 30);
+			sscanf(cmd_line + pos, "%s", buff);
+			last = AddDirectory(last, buff);
+			break;
+		case 2://cd..
+			last = ParentDirectory(last, &stack);
+			break;
+		case 3://cd Dir
+			memset(buff, 0, 30);
+			sscanf(cmd_line + pos, "%s", buff);
+			last = Subdirectory(last->child, buff, &stack);
+			break;
+		case 4://Dir
+			if (SubdirectoryPrint(last) != 0)
+				printf("An error occured!\n");
+			break;
+		case 5://exit
+			break;
 		default:
+			printf("%s is not recognized as an internal or external command, operable program or batch file.", buff);
 			break;
 		}
+	}
+
+	free(cmd_line);
+	free(buff);
+
+	return 0;
+}
+
+Position ParentDirectory(Position last, StackPosition stack)
+{
+	StackPosition tmp = NULL;
+	Position temp = NULL;
+
+	tmp = stack->next;
+	stack->next = tmp->next;
+
+	temp = stack->next->Dir;
+	free(tmp);
+
+	return temp;
+}
+
+Position Subdirectory(Position lastchild, char* buff, StackPosition stack)
+{
+	Position current = NULL;
+
+	current = lastchild;
+
+	while (strcmp(current->dirname, buff) && current != NULL)
+		current = current->sibling;
+
+
+	if (Push(current, stack) != 0)
+		printf("An error occured!\n");
+
+	return current;
+}
+
+int Push(Position current, StackPosition stack)
+{
+	StackPosition NewNode = NULL;
+
+	NewNode = (StackPosition)malloc(sizeof(Stack));
+	if (NULL == NewNode)
+		printf("An error occured!\n");
+
+	NewNode->next = NULL;
+	NewNode->Dir = NULL;
+
+
+	NewNode->Dir = current;
+	NewNode->next = stack->next;
+	stack->next = NewNode;
+
+	return 0;
+}
+
+int SubdirectoryPrint(Position last)
+{
+	Position current = NULL;
+
+	current = last->child;
+
+	printf("\nDirectory of %s\n\n", last->dirname);
+
+	while (current != NULL)
+	{
+		printf("<Dir>\t%s\n", current->dirname);
+		current = current->sibling;
 	}
 
 	return 0;
 }
 
-void MakeDirectory(Position p){
-	Position newDir = CreateNewDirectory();
-	if (NULL == p->child) {
-		p->child = newDir;
-		newDir->next = NULL;
+Position AddDirectory(Position last, char* dirname)
+{
+	Position newKnot = NULL;
+	Position previous = NULL;
+
+	newKnot = (Position)malloc(sizeof(Dir));
+	if (NULL == newKnot)
+		printf("An error occured!\n");
+
+	newKnot->dirname = (char*)malloc(30 * sizeof(char));
+	if (NULL == newKnot->dirname)
+		printf("An error occured!\n");
+
+	strcpy(newKnot->dirname, dirname);
+	newKnot->child = NULL;
+	newKnot->sibling = NULL;
+
+	if (NULL == last->child)
+	{
+		newKnot->child = last->child;
+		last->child = newKnot;
+
+		return last;
 	}
-	CreateSortedList(newDir, p->child);
+
+	previous = last->child;
+
+	while (previous->sibling != NULL && (strcmp(previous->sibling->dirname, newKnot->dirname) < 0))
+		previous = previous->sibling;
+
+	if (previous != NULL && (strcmp(previous->dirname, newKnot->dirname) < 0))
+	{
+		newKnot->sibling = previous->sibling;
+		previous->sibling = newKnot;
+
+		return last;
+	}
+	else
+	{
+		newKnot->sibling = last->child;
+		last->child = newKnot;
+
+		return last;
+	}
 }
-Position CreateNewDirectory() {
-	Position dir = NULL;
-	char* name = "";
-	dir = (Position)malloc(sizeof(Dir));
-	if (NULL == dir) {
-		err;
-		return NULL;
-	}
-	puts("Unesite ime novog direktorija: ");
-	while ("" == name) {
-		scanf(" %s", name);
-	}
-	strcpy(dir->Name, name);
-	dir->child = NULL;
-	return dir;
-}
-void CreateSortedList(Position newDir, Position p) {
-	if (NULL == p->child->next) {
-		if(strcmp(p->child->Name, newDir->Name)){
-			
-		}
-	}
+
+int MenuChoice(char* choice)
+{
+	if (!strcmp(choice, "md"))
+		return 1;
+	else if (!strcmp(choice, "cd.."))
+		return 2;
+	else if (!strcmp(choice, "cd"))
+		return 3;
+	else if (!strcmp(choice, "Dir"))
+		return 4;
+	else if (!strcmp(choice, "exit"));
+	return 5;
 }
